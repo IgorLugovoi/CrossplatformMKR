1b6a6340c366d1554dabfa3b3dffdc61568ffd5f
IgorLugovoi <qeadzc4065@knu.ua>
2024-11-13 10:38:19 +0200

    Добавьте файлы проекта.

---
+++ CrossplatformMKR/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CrossplatformMKR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 task = new Task1();
+            int result = task.TaskSolution();
+            //File.WriteAllText(@"MKR\output.txt", $"{result}");
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\output.txt"), $"{result}");
+        }
+    }
+}
